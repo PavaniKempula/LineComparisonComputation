e19f0efc9b65fc7c11292856199731ac9416bf51
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-26 22:29:56 +0530

    ADDED : Welcome message to Line Comparison Computation

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		//Displaying Welcome Message for User
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
